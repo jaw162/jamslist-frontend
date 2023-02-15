ee8e24e875c8da8f9b8678be354853e882b06ed3
jaw162 <jwhite162@protonmail.com>
2023-02-15 11:27:18 +0000

    added backend url

--- config.ts
+++ config.ts
@@ -1,2 +1,4 @@
 export const API_URL =
-  process.env.NODE_ENV === "development" ? "http://localhost:3001" : "";
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3001"
+    : "https://jamslist-backend.herokuapp.com";
