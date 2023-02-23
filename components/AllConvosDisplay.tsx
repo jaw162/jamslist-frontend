af11777800fe524a9310e4152b632d9443085ec3
jaw162 <jwhite162@protonmail.com>
2023-02-23 13:34:39 +0000

    changed messages display

--- components/AllConvosDisplay.tsx
+++ components/AllConvosDisplay.tsx
@@ -29,6 +29,8 @@ export default function AllConvosDisplay({
       {messages.length
         ? messages.map(el => {
             const sentAt = new Date(el.messages[0].createdAt);
+            const [otherUser] = el.user.filter(user => user.id !== userId);
+
             return (
               <Flex
                 key={el.id}
@@ -56,7 +58,7 @@ export default function AllConvosDisplay({
                   borderRadius={"5px"}
                 >
                   <Box>
-                    <Box>{el.user[0].username}</Box>
+                    <Box>{otherUser.username}</Box>
                     <Box>{el.messages[0].title}</Box>
                     <Box>{el.messages[0].content}</Box>
                     <Box>{date.format(sentAt, "DD/MM/YY MM:HH")}</Box>
